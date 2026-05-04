d5ec591c663869bd4cffa689035fd6211b5e615a
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-04 19:26:44 +0200

    feat(main): add new farm routines and assign to characters
    Added ash_farm, gudgeon_farm, and sunflower_farm routines. Assigned these
    new routines to alice, jane, and charlie respectively. Also added john
    with the copper_farm routine. Updated asyncio.gather to include all
    characters' work methods.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!.venv/bin/python
 from models import Character
-from routines import chicken_farm, copper_farm
+from routines import chicken_farm, copper_farm, ash_farm, gudgeon_farm, sunflower_farm
 import asyncio
 
 
@@ -9,11 +9,23 @@ async def main():
     bob.assign_routine(chicken_farm)
 
     alice = Character("rs_alice")
-    alice.assign_routine(copper_farm)
+    alice.assign_routine(ash_farm)
+
+    john = Character("rs_john")
+    john.assign_routine(copper_farm)
+
+    jane = Character("rs_jane")
+    jane.assign_routine(gudgeon_farm)
+
+    charlie = Character("rs_charlie")
+    charlie.assign_routine(sunflower_farm)
 
     _ = await asyncio.gather(
         bob.work(),
         alice.work(),
+        john.work(),
+        jane.work(),
+        charlie.work(),
     )
 
 
