9fb4fb9df949b4d8ba00432cdffc181bff17f883
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-04 22:14:27 +0200

    feat(routines): add yellow_slime_farm to exports
    Exported yellow_slime_farm from routines package and updated __all__ to
    include it. This makes yellow_slime_farm available for import from the
    routines module.

--- routines/__init__.py
+++ routines/__init__.py
@@ -1,8 +1,15 @@
-from .monster_farm import chicken_farm
+from .monster_farm import chicken_farm, yellow_slime_farm
 from .ore_farm import copper_farm
 from .wood_farm import ash_farm
 from .fish_farm import gudgeon_farm
 from .alchemy_farm import sunflower_farm
 
 
-__all__ = ["chicken_farm", "copper_farm", "ash_farm", "gudgeon_farm", "sunflower_farm"]
+__all__ = [
+    "chicken_farm",
+    "copper_farm",
+    "ash_farm",
+    "gudgeon_farm",
+    "sunflower_farm",
+    "yellow_slime_farm",
+]
