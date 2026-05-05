e0877896ffe0068f3715c634ba8d753b12bbcb5f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-05 13:44:47 +0200

    feat(core): add graceful shutdown for asyncio tasks
    Refactored main.py to handle SIGINT and SIGTERM signals, enabling
    graceful shutdown of all running asyncio tasks. This improves program
    stability and ensures resources are properly released on exit.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!.venv/bin/python
-from models import Character, GameManager
-from utils.cli import cli
-import routines
 import asyncio
+import signal
+import sys
+
+import routines
+from models import GameManager
+from utils.cli import cli
 
 
 async def main():
@@ -22,11 +25,28 @@ async def main():
         else:
             print(f"No default task found for {char_name}, skipping task assignment.")
 
-    _ = await asyncio.gather(
+    loop = asyncio.get_event_loop()
+    tasks = asyncio.gather(
         *[char.work() for char in characters.values()],
         cli(characters),
     )
 
+    def shutdown():
+        print("Shutting down gracefully...")
+        _ = tasks.cancel()
+
+    loop.add_signal_handler(signal.SIGINT, shutdown)
+    loop.add_signal_handler(signal.SIGTERM, shutdown)
+
+    try:
+        await tasks
+    except asyncio.CancelledError:
+        raise
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except asyncio.CancelledError:
+        print("Program terminated.")
+        sys.exit(0)
