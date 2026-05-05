bf6550f67b050d566e29156061498181be39bc58
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-05 21:00:14 +0200

    feat(craft): split crafting into multiple trips if needed
    Refactor the craft routine to handle cases where the character's
    inventory cannot hold all required ingredients for the requested
    quantity. The function now calculates the number of trips needed and
    splits the crafting process accordingly, ensuring inventory limits are
    respected. Adds informative output for trip planning and completion.

--- routines/craft.py
+++ routines/craft.py
@@ -5,19 +5,25 @@ async def craft(
     character: Character, workshop_location: tuple[int, int], item: Item, quantity: int
 ):
     ingredients = [
-        (str(ingredient["code"]), int(ingredient["quantity"]) * quantity)
+        (str(ingredient["code"]), int(ingredient["quantity"]))
         for ingredient in item.craft_ingredients
     ]
     nb_ingredients = sum(nb[1] for nb in ingredients)
-    if character.inventory_max_items < nb_ingredients:
-        print(
-            f"❌ {character.surname} does not have enough inventory space to craft {quantity}x {item.name}"
-        )
-        return
+    nb_trips = max((nb_ingredients * quantity) // character.inventory_max_items, 1)
+    nb_craft_per_trip = quantity // nb_trips
 
-    _ = await character.deposit_all_in_bank(comeback=False)
-    if await character.withdraw_item_from_bank(ingredients):
-        if await character.move(workshop_location):
-            _ = await character.craft(item, quantity)
+    ingredients_for_trip = [
+        (code, nb_craft_per_trip * quantity) for code, quantity in ingredients
+    ]
+
+    print(
+        f"⚒️ {character.surname} needs to make {nb_trips} trips to craft {quantity}x {item.name}"
+    )
+
+    for _ in range(nb_trips):
+        _ = await character.deposit_all_in_bank(comeback=False)
+        if await character.withdraw_item_from_bank(ingredients_for_trip):
+            if await character.move(workshop_location):
+                _ = await character.craft(item, nb_craft_per_trip)
 
     print(f"⚒️ {character.surname} finished crafting {quantity}x {item.name}")
