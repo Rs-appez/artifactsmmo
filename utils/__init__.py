8fcf5454bb862c7847d5f36743fb39d17b4b74c2
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-06 15:02:39 +0200

    Revert "feat(utils): add find_craft_routine utility"
    This reverts commit 7ea2ed7cc9cfb009463da5d1194f774274f83d4b.

--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .find_craft_routine import find_craft_routine
-
-__all__ = ["find_craft_routine"]
--- utils/find_craft_routine.py
+++
@@ -1,9 +0,0 @@
-from typing import Callable
-from models import Item
-import routines
-
-craft_mapping = {"cooking": routines.craft_cooking}
-
-
-def find_craft_routine(item: Item) -> Callable | None:
-    return craft_mapping.get(item.job, None)
