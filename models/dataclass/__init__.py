5535366f79a2a86a963e770c64e23f3c6a06d628
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-08 00:21:40 +0200

    feat(models): add Monster dataclass and init exports
    - Introduced the `Monster` dataclass in `models/dataclass/monsters.py` with
      attributes for stats, resistances, gold, and drops.
    - Implemented `from_dict` classmethod for easy instantiation from dict data.
    - Updated `models/dataclass/__init__.py` to export `Item`, `Monster`, and
      `Effect` for easier imports.

---
+++ models/dataclass/__init__.py
@@ -0,0 +1,5 @@
+from .items import Item
+from .monsters import Monster
+from .effects import Effect
+
+__all__ = ["Item", "Monster", "Effect"]
---
+++ models/dataclass/monsters.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+
+from models import GameManager
+from models.dataclass import Effect, Item
+
+
+@dataclass(frozen=True)
+class Monster:
+    name: str
+    code: str
+    level: int
+    type: str
+    hp: int
+    attack_fire: int
+    attack_water: int
+    attack_earth: int
+    attack_air: int
+    res_fire: int
+    res_water: int
+    res_earth: int
+    res_air: int
+    critical_strike: int
+    initiative: int
+    # effects: list[tuple[Effect, int]]
+    min_gold: int
+    max_gold: int
+    drops: list[dict[Item, dict[str, str | int]]]
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            name=data["name"],
+            code=data["code"],
+            level=data["level"],
+            type=data["type"],
+            hp=data["hp"],
+            attack_fire=data["attack_fire"],
+            attack_water=data["attack_water"],
+            attack_earth=data["attack_earth"],
+            attack_air=data["attack_air"],
+            res_fire=data["res_fire"],
+            res_water=data["res_water"],
+            res_earth=data["res_earth"],
+            res_air=data["res_air"],
+            critical_strike=data["critical_strike"],
+            initiative=data["initiative"],
+            # effects=[
+            #     (
+            #         GameManager.get_effect_by_code(effect_data["code"]),
+            #         effect_data["value"],
+            #     )
+            #     for effect_data in data.get("effects", [])
+            # ],
+            min_gold=data["min_gold"],
+            max_gold=data["max_gold"],
+            drops=[
+                {
+                    GameManager.items[drop_data["item_code"]]: {  # noqa: F821
+                        "chance": drop_data["chance"],
+                        "quantity": drop_data.get("quantity", 1),
+                    }
+                }
+                for drop_data in data.get("drops", [])
+            ],
+        )
