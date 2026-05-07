78812221f809d98ceac677ec4b0e8f73476bde45
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-08 00:05:39 +0200

    feat(locations): add monster_locations mapping (WIP)
    Added a new `monster_locations` dictionary in `locations/monsters.py` with
    coordinates for various monsters. Updated `locations/__init__.py` to export
    `monster_locations` in `__all__` for easier imports.

--- locations/__init__.py
+++ locations/__init__.py
@@ -1,4 +1,5 @@
 from .workshop import workshop_locations
 from .mics import bank_locations
+from .monsters import monster_locations
 
-__all__ = ["workshop_locations", "bank_locations"]
+__all__ = ["workshop_locations", "bank_locations", "monster_locations"]
---
+++ locations/monsters.py
@@ -0,0 +1,11 @@
+monster_locations = {
+    "chicken": [(0, 1)],
+    "sheep": [(5, 12)],
+    "cow": [(0, 2)],
+    "pig": [(-3, -3)],
+    "yellow_slime": [(4, -1)],
+    "green_slime": [(3, -2)],
+    "blue_slime": [(2, -1)],
+    "red_slime": [(1, -1), (2, -2)],
+    "mushroom": [(5, 3), (6, 4)],
+}
