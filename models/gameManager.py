748a89b630e4e54e3673c8b37c519aca994e84f1
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-07 12:29:59 +0200

    feat(game): update default tasks for characters
    Changed the default tasks assigned to characters in GameManager. Updated
    "bob" to red_slime_farm, "alice" to ash_farm, "john" and "jane" to
    copper_farm, and "charlie" to sheep_farm for improved task distribution.

--- models/gameManager.py
+++ models/gameManager.py
@@ -10,11 +10,11 @@ from models import Character, CharacterData, Item
 
 class GameManager:
     default_tasks = {
-        "bob": routines.sheep_farm,
-        "alice": routines.blue_slime_farm,
-        "john": routines.green_slime_farm,
-        "jane": routines.yellow_slime_farm,
-        "charlie": routines.red_slime_farm,
+        "bob": routines.red_slime_farm,
+        "alice": routines.ash_farm,
+        "john": routines.copper_farm,
+        "jane": routines.copper_farm,
+        "charlie": routines.sheep_farm,
     }
 
     jobs = {
