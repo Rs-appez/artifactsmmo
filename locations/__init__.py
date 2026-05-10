bd012975de3aa3dcd6eba20277a808cb5105b42f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-10 23:18:06 +0200

    feat(locations): add mining_locations to exports
    Added mining_locations import and included it in the __all__ list for
    the locations package, making mining location data available for
    external use.

--- locations/__init__.py
+++ locations/__init__.py
@@ -2,6 +2,7 @@ from .workshop import workshop_locations
 from .mics import bank_locations
 from .monsters import monster_locations
 from .task import monster_task_master, item_task_master
+from .resources import mining_locations
 
 __all__ = [
     "workshop_locations",
@@ -9,4 +10,5 @@ __all__ = [
     "monster_locations",
     "monster_task_master",
     "item_task_master",
+    "mining_locations",
 ]
