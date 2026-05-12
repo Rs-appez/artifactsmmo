1284bd3c37ef2e9060d0f4047d98763a132de1e6
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-13 01:28:40 +0200

    refactor(locations): remove unused location modules
    Removed workshop.py, mics.py, and monsters.py from the locations package,
    along with their imports and __all__ entries in __init__.py. These modules
    are no longer used and their data has been consolidated elsewhere.

--- locations/__init__.py
+++ locations/__init__.py
@@ -1,13 +1,7 @@
-from .workshop import workshop_locations
-from .mics import bank_locations
-from .monsters import monster_locations
 from .task import monster_task_master, item_task_master
 from .resources import mining_locations
 
 __all__ = [
-    "workshop_locations",
-    "bank_locations",
-    "monster_locations",
     "monster_task_master",
     "item_task_master",
     "mining_locations",
--- locations/workshop.py
+++
@@ -1,9 +0,0 @@
-workshop_locations = {
-    "woodcutting": [(-2, -3)],
-    "mining": [(1, 5)],
-    "cooking": [(1, 1)],
-    "weaponcrafting": [(2, 1)],
-    "gearcrafting": [(3, 1)],
-    "jewelrycrafting": [(1, 3)],
-    "alchemy": [(2, 3)],
-}
