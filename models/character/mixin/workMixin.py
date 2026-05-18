643b48120b366bd48877510d2b5ff74abb5f370e
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-18 18:17:21 +0200

    Revert "fix(work): always interrupt routine on one-time task"
    This reverts commit abef2ee00ff4d9533418ea5459cbd3a5cc813d4b.

--- models/character/mixin/workMixin.py
+++ models/character/mixin/workMixin.py
@@ -74,7 +74,8 @@ class WorkMixin(Protocol):
 
     def do_one_time_task(self: "Character", task: Callable):
         self._priority_tasks.append(task)
-        self._interrupt_routine()
+        if len(self._priority_tasks) == 1:
+            self._interrupt_routine()
 
     def stop(self: "Character"):
         self._previous_routine = self._routine
@@ -95,7 +96,7 @@ class WorkMixin(Protocol):
             self._interrupt_routine()
 
     def _interrupt_routine(self: "Character"):
-        if self._work_task is not None and len(self._priority_tasks) == 1:
+        if self._work_task is not None:
             self._interrupted = True
             _ = self._work_task.cancel()
 
