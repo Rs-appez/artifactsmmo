7442edbd83d40dfdd136c737bafd53cd9670819b
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-18 16:19:25 +0200

    feat(routine): add repeat count to gather function
    The gather routine now accepts an optional `nb` parameter to specify
    how many times to repeat the gathering process. If `nb` is not
    provided or set to a negative value, gathering will repeat
    indefinitely. This change enables batch resource collection and
    improves automation flexibility.

--- routines/gather.py
+++ routines/gather.py
@@ -1,15 +1,19 @@
+from itertools import count
 from models import Character, Encyclopedia
 from models.dataclass import Item, Resource
 from utils.find_nearest import find_nearest_lootable
 
 
-async def gather(character: Character, item: Item | str) -> None:
+async def gather(character: Character, item: Item | str, nb: int = -1) -> None:
     if isinstance(item, str):
         item = await Encyclopedia.get_item_by_code(item)
     await character.toolize(item.job)
-    if character.is_inventory_full:
-        _ = await character.deposit_all_in_bank()
-    resources = Resource.from_drop_item(item)
-    resource_position = await find_nearest_lootable(character, resources)
-    _ = await character.move(resource_position)
-    _ = await character.gather()
+
+    iterations = range(nb) if nb > 0 else count()
+    for _ in iterations:
+        if character.is_inventory_full:
+            _ = await character.deposit_all_in_bank()
+        resources = Resource.from_drop_item(item)
+        resource_position = await find_nearest_lootable(character, set(resources))
+        _ = await character.move(resource_position)
+        _ = await character.gather()
