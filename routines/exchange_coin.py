269da95c22127bc7a35afe7bf6262473eacfa0d8
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-24 02:43:30 +0200

    refactor(exchange): improve task coin exchange logic
    Refactored the exchange_task_coin routine to use Encyclopedia for fetching
    the task coin item and updated the logic to repeatedly process task coin
    exchanges while available. Improved error handling and replaced Bank
    methods with get_max_items for better clarity and maintainability.

--- routines/exchange_coin.py
+++ routines/exchange_coin.py
@@ -1,13 +1,26 @@
-from models import Character
-from models.dataclass.bank import Bank
+from models import Character, Encyclopedia
+from models.dataclass.bank import get_max_items
 from utils.find_nearest import find_nearest_tasks_master
 
 
 async def exchange_task_coin(character: Character) -> None:
 
-    async with Bank.get_max_item(character, "task_coin") as token:
+    task_coin = await Encyclopedia.get_item_by_code("task_coin")
+    task_master_pos = await find_nearest_tasks_master(character)
 
-    task_master_pos = find_nearest_tasks_master(character)
-    if not await character.move(task_master_pos):
-        print("Failed to move to task master")
-        return
+    while True:
+        async with get_max_items(character, task_coin) as (token, token_quantity):
+            if token_quantity < 6:
+                print("No more task coin in bank")
+                break
+            if not await character.deposit_all_in_bank(with_gold=False):
+                print("Failed to deposit task coin in bank")
+                return
+            if not await character.withdraw_item_from_bank(token):
+                print("Failed to withdraw task coin from bank")
+                return
+            if not await character.move(task_master_pos):
+                print("Failed to move to task master")
+            while await character.exchange_task_coin():
+                pass
+            return
