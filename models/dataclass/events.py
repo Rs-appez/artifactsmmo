7a742c6f35e96dffdddcfdbb4b20ace55df60f6f
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-26 18:16:38 +0200

    feat(events): support NPC and None as event content
    Extend the Event dataclass to allow content to be a Monster, Resource,
    NPC, or None. Update from_dict to handle different content types using
    pattern matching. Add placeholder for NPC handling, to be implemented
    when NPC data becomes available in the encyclopedia.

--- models/dataclass/events.py
+++ models/dataclass/events.py
@@ -2,14 +2,14 @@ from dataclasses import dataclass
 from typing import override
 
 from models import Encyclopedia, LocationRegistry
-from models.dataclass import Map, Monster, Resource
+from models.dataclass import NPC, Map, Monster, Resource
 
 
 @dataclass(frozen=True)
 class Event:
     name: str
     code: str
-    content: Monster | Resource
+    content: Monster | Resource | NPC | None
     maps: list[Map]
 
     @override
@@ -18,11 +18,27 @@ class Event:
 
     @classmethod
     async def from_dict(cls, data):
+        content = None
+        match data["content"]["type"]:
+            case "monster":
+                content = await Encyclopedia.get_monster_by_code(
+                    data["content"]["code"]
+                )
+            case "resource":
+                content = await Encyclopedia.get_resource_by_code(
+                    data["content"]["code"]
+                )
+            case "npc":
+                pass
+                # TODO: NPC data is not yet available in the encyclopedia, so this will be implemented later.
+                # content = await Encyclopedia.get_npc_by_code(data["content"]["code"])
+            case _:
+                pass
 
         return cls(
             name=data["name"],
             code=data["code"],
-            content=await Encyclopedia.get_monster_by_code(data["content"]),
+            content=content,
             maps=[
                 await LocationRegistry.get_map_by_id(map_code)
                 for map_code in data["maps"]["map_id"]
