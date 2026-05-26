350e240f377985437da617a6d3c444e5446542c3
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-26 18:45:35 +0200

    feat(events): add async event refresh and registry sync
    Implement async methods in EventHandler to fetch current events from the
    API and synchronize them with the LocationRegistry. This includes
    removing old event locations and adding new ones, with error handling
    and logging for missing or failed event retrieval.

--- models/eventHandler.py
+++ models/eventHandler.py
@@ -1,3 +1,40 @@
+from models.dataclass import Map, Event
+from models import Encyclopedia, LocationRegistry
+from httpx import AsyncClient
+from config import ARTIFACTSMMO_URL, HEADERS
+
+
 class EventHandler:
-    def __get_current_events(self):
-        pass
+    current_events: list[tuple[Event, Map]] = []
+
+    async def refresh_current_events(self) -> None:
+        for event in self.current_events:
+            LocationRegistry.remove_event_location(event[1], event[0])
+
+        self.current_events = await self.__get_current_events()
+        if not self.current_events:
+            print("No active events found.")
+            return
+
+        for event, map_location in self.current_events:
+            LocationRegistry.add_event_location(map_location, event)
+
+    async def __get_current_events(self) -> list[tuple[Event, Map]]:
+        try:
+            async with AsyncClient() as client:
+                response = await client.get(
+                    f"{ARTIFACTSMMO_URL}/events/active", headers=HEADERS
+                )
+                _ = response.raise_for_status()
+                events_data = response.json()
+                return [
+                    (
+                        await Encyclopedia.get_event_by_code(event["code"]),
+                        await LocationRegistry.get_map_by_id(map_event["map_id"]),
+                    )
+                    for map_event in events_data["maps"]
+                    for event in events_data
+                ]
+        except Exception as e:
+            print(f"Error fetching current events: {e}")
+            return []
