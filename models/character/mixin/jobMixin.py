79aa6502959147f2720b2ed44f019f2654be0117
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-05-31 01:42:45 +0200

    feat(character): add will_gain_xp_with to JobMixin
    Introduce the will_gain_xp_with method to JobMixin, allowing checks if a
    character will gain XP from crafting an item or resource. This method
    compares the character's job level with the item's required level and
    returns False if the level difference is 11 or more.

--- models/character/mixin/jobMixin.py
+++ models/character/mixin/jobMixin.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Protocol
 
+from models.dataclass import Item, Resource
 from models.enums import JobType
 
 if TYPE_CHECKING:
@@ -18,3 +19,10 @@ class JobMixin(Protocol):
     def has_job(self: "Character", job_name: str | JobType, level=1) -> bool:
         job = JobType(job_name) if isinstance(job_name, str) else job_name
         return self._jobs.get(job, 0) >= level
+
+    def will_gain_xp_with(self: "Character", item: Item | Resource) -> bool:
+        job = item.job if isinstance(item, Item) else item.skill
+        item_level = item.craft_level if isinstance(item, Item) else item.level
+        if self.get_job_level(job) >= item_level + 11:
+            return False
+        return True
