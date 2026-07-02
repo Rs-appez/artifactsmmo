2d0b1081d3fb1b792f871646c60677ad411646b0
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-02 20:44:58 +0200

    refactor(api): add retry logic for API requests
    Retry logic added to handle transient errors during API calls. This ensures the request is retried up to three times with exponential backoff if it fails initially.

--- models/character/mixin/apiMixin.py
+++ models/character/mixin/apiMixin.py
@@ -1,3 +1,4 @@
+import asyncio
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
@@ -35,22 +36,33 @@ class ApiMixin:
     async def post_api(
         self: "Character", endpoint: str, json: dict | list[dict] | None = None
     ) -> dict:
-        response = await self.client.post(endpoint, json=json)
-        data = response.json()
+        attempt = 0
+        while attempt < 3:
+            try:
+                response = await self.client.post(endpoint, json=json, timeout=2.0)
+                data = response.json()
 
-        if "error" in data:
-            print("data : ", data)
-            raise Exception(data["error"]["message"])
+                if "error" in data:
+                    print("data : ", data)
+                    raise Exception(data["error"]["message"])
 
-        data = data["data"]
-        character_data = {}
-        if "character" not in data:
-            characters = data["characters"]
-            for character in characters:
-                if character["name"] == self.name:
-                    character_data = character
-                    break
+                data = data["data"]
+                character_data = {}
+                if "character" not in data:
+                    characters = data["characters"]
+                    for character in characters:
+                        if character["name"] == self.name:
+                            character_data = character
+                            break
+                else:
+                    character_data = data["character"]
+
+                break
+            except httpx.RequestError as e:
+                print(f"Attempt {attempt} failed: {e}. Retrying...")
+                await asyncio.sleep(2**attempt)
+                attempt += 1
         else:
-            character_data = data["character"]
+            raise Exception("Failed to post API after 3 attempts.")
 
         return character_data
