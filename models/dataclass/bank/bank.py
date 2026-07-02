fb153e96badd857b5a1eed7a4c1293ca10c3fbe0
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-02 11:11:00 +0200

    fix(bank): add timeout to bank API calls

--- models/dataclass/bank/bank.py
+++ models/dataclass/bank/bank.py
@@ -200,7 +200,7 @@ class Bank:
     async def __get_details(cls) -> dict:
         try:
             async with AsyncClient() as client:
-                response = await client.get(cls.__url, headers=HEADERS)
+                response = await client.get(cls.__url, headers=HEADERS, timeout=1.0)
                 data = response.json()
                 if "error" in data:
                     raise Exception(data["error"]["message"])
@@ -220,6 +220,7 @@ class Bank:
                         f"{cls.__url}/items",
                         headers=HEADERS,
                         params={"size": 100, "page": page},
+                        timeout=1.0,
                     )
                     data = response.json()
                     if "error" in data:
