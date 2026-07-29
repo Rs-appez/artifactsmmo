613cf9a0f123250aa6ab0f02a0110c339f9419c2
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-29 15:55:21 +0200

    refactor(effects): replace element parsing with properties
    Refactored `Effect` class to use properties for element extraction. Removed `utils/elements.py` as its functionality is now integrated into `models/dataclass/effects.py`. Updated `score_weapon` and `score_equipment` functions to utilize these new properties, improving readability and performance.

--- models/dataclass/effects.py
+++ models/dataclass/effects.py
@@ -29,10 +29,32 @@ class Effect:
     @property
     def has_element(self) -> bool:
         """Returns True if the effect has an associated element, False otherwise."""
-        return self.get_element() is not None
+        return self.get_element is not None
 
+    @property
     def get_element(self) -> Element | None:
         """Returns the element associated with the effect, if any."""
         for element in Element:
             if self.code.endswith(element.value):
+                return element
+
+    @property
+    def get_atk_element(self) -> Element | None:
+        """Returns the element associated with the effect's attack, if any."""
+        for element in Element:
+            if self.code.startswith(f"atk_{element.value}"):
+                return element
+
+    @property
+    def get_res_element(self) -> Element | None:
+        """Returns the element associated with the effect's resistance, if any."""
+        for element in Element:
+            if self.code.startswith(f"res_{element.value}"):
+                return element
+
+    @property
+    def get_dmg_element(self) -> Element | None:
+        """Returns the element associated with the effect's damage, if any."""
+        for element in Element:
+            if self.code.startswith(f"dmg_{element.value}"):
                 return element
