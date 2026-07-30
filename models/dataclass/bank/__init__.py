44c2da7caacee9acc8d6c3c95a5e0ae8da2ccfbb
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-30 11:39:44 +0200

    refactor(bank): reorder imports and add async context manager for equipment retrieval
    Reordered imports to improve readability and added an async context manager to handle the retrieval of best equipment for a given monster. This change enhances the modularity and maintainability of the bank module.

--- models/dataclass/bank/__init__.py
+++ models/dataclass/bank/__init__.py
@@ -1,5 +1,12 @@
 from .bank import Bank, lock_bank
-from .get_in_bank import get_food, get_tool, get_max_items, get_bag, get_best_stat_item
+from .get_in_bank import (
+    get_bag,
+    get_best_equipment,
+    get_best_stat_item,
+    get_food,
+    get_max_items,
+    get_tool,
+)
 
 __all__ = [
     "Bank",
@@ -9,4 +16,5 @@ __all__ = [
     "get_max_items",
     "get_bag",
     "get_best_stat_item",
+    "get_best_equipment",
 ]
