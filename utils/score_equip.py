650a995aa10757fa49e63425e9743c084c7f2ca5
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-07-31 12:20:53 +0200

    refactor(utils/score_equip.py): update weights for better scoring logic

--- utils/score_equip.py
+++ utils/score_equip.py
@@ -7,18 +7,27 @@ if TYPE_CHECKING:
     from models.dataclass import Item, Monster
 
 TOP_K_WEAPONS = 5
-
 WEIGHTS = {
-    "dmg": 2.0,
-    "elemental_dmg": 2.0,
-    "elemental_res": 1.0,
-    "hp": 0.5,
-    "critical_strike": 1.0,
-    "haste": 0.05,
-    "initiative": 0.5,
-    "prospecting": 0.1,
-    "inventory_space": 0.01,
+    "dmg": 1.274033258119729,
+    "elemental_dmg": 0.08583063944661747,
+    "elemental_res": 0.050624965014169535,
+    "hp": 0.1325939823954532,
+    "critical_strike": 0.9789483154407386,
+    "initiative": 0.13160696417676848,
+    "prospecting": 0.03224571781920886,
+    "inventory_space": 0.000001,
+    "haste": 0.000001,
 }
+# mine
+# WEIGHTS = {
+#     "dmg": 2.0,
+#     "elemental_dmg": 2.0,
+#     "elemental_res": 1.0,
+#     "hp": 0.5,
+#     "critical_strike": 1.0,
+#     "initiative": 0.5,
+#     "prospecting": 0.01,
+# }
 
 
 def _damage_multiplier(element: Element, monster: "Monster") -> float:
