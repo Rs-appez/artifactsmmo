f9b69aeff6695fec2080398fedfe8a71e5bfd6db
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-08-02 17:39:24 +0200

    refactor(score_equip): add rune check to filter equipment
    Refactor the `_best_equips_for_monster` function to include a check for `item.is_rune`, ensuring that runes are not considered as equipment. This change improves the accuracy of the equipment scoring by excluding runes from the consideration.

--- utils/score_equip.py
+++ utils/score_equip.py
@@ -157,7 +157,12 @@ def _best_equips_for_monster(
 
     armor_by_slot: dict[str, list[tuple["Item", int]]] = {}
     for item, qty in items:
-        if item.is_equipment and not item.is_weapon and not item.is_tool:
+        if (
+            item.is_equipment
+            and not item.is_weapon
+            and not item.is_tool
+            and not item.is_rune
+        ):
             armor_by_slot.setdefault(item.type, []).append((item, qty))
 
     armor_by_slot_set = frozenset(frozenset(v) for v in armor_by_slot.values())
