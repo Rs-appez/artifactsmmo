df74596efeeab719c5560bd6c9c40b1b23138990
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-08-05 15:28:52 +0200

    fix(characterManager): remove unnecessary condition for mob farm work on

--- models/characterManager.py
+++ models/characterManager.py
@@ -20,9 +20,5 @@ class CharacterManager:
             return
         item = min(event.content.drops.items(), key=lambda d: d[1]["rate"])[0]
         for character in self.characters.values():
-            if (
-                character.has_job(item.job, item.level)
-                and character.work_on
-                != "mob_farm"  # TODO : tmp before handle restuff for fighting
-            ):
+            if character.has_job(item.job, item.level) and character.work_on:
                 character.do_one_time_task(gather, item)
